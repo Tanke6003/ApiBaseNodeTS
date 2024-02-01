a5a3e9f22712638f96ff432f069b575666b50b89
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2024-01-31 21:57:11 -0600

    create token

--- src/presentation/routes/index.routes.ts
+++ src/presentation/routes/index.routes.ts
@@ -1,31 +1,37 @@
 // index.ts
 
-import { Router } from 'express';
-import { UserModule } from './user.routes';
+import { Router } from "express";
+import { UserModule } from "./user.routes";
+import { AuthModule } from "./auth.routes";
 
+class RouterModule {
+  router: Router = Router();
+  routes: Router[] = [];
 
-class RouterModule{
-  router:Router = Router();
-  routes:Router[]= [];
-
-  constructor(){
+  constructor() {
     this.setRoutes();
   }
-  setRoutes(){
-  /**
-   * @swagger
-   * tags:
-   *   name: Users
-   *   description: Operaciones relacionadas con usuarios
-   */
+  setRoutes() {
+    /**
+     * @swagger
+     * tags:
+     *   name: Users
+     *   description: Operaciones relacionadas con usuarios
+     */
     const userModule = new UserModule(this.router);
     this.routes.push(userModule.getRoutes());
-
+    /**
+     * @swagger
+     * tags:
+     *   name: Auth
+     *   description: Operaciones relacionadas con la autenticación
+     */
+    const authModule = new AuthModule(this.router);
+    this.routes.push(authModule.getRoutes());
   }
-  getRoutes(){
-    return this.routes
+  getRoutes() {
+    return this.routes;
   }
 }
 
-
-export { RouterModule};
+export { RouterModule };
